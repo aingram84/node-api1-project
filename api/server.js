95bf17a2df87af6d4dcb7d7bb644c1963234b701
Adam Ingram <kr0nai84@gmail.com>
2024-03-12 12:26:30 -0700

    Server established and responsive

--- api/server.js
+++ api/server.js
@@ -1,3 +1,21 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+server.get('/api/users', async(req, res) => {
+    try{
+        const users = await User.find()
+        res.status(200).json(users)
+    }
+    catch(error){
+        res.status(500).json({
+            message: `The user's information could nto be retrieved`
+        })
+    }
+})
+
+
+module.exports = server;
